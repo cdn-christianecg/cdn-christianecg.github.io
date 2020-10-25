edf8232855c285bb67af1e56f1af7d9e12e04439
Christian Elías <contacto@christianecg.com>
2020-10-24 20:09:00 -0500

    Add content in an async function

--- js/name.js
+++ js/name.js
@@ -3,15 +3,20 @@ var form = document.getElementById('form');
 var res = document.getElementById('res');
 
 form.addEventListener('submit', (event) => {
-    var name = document.getElementById('name').value;
     event.preventDefault();
+    getName();
+});
 
+async function getName() {
+    var name = document.getElementById('name').value;
     console.log(
         'https://node-red-christianecg.mybluemix.net/name_api?name=' + name
-	);
-	
-	var response = await fetch('https://node-red-christianecg.mybluemix.net/name_api?name='+name);
-	var data = await response.json();
+    );
 
-	res.innerHTML = data;
-});
+    var response = await fetch(
+        'https://node-red-christianecg.mybluemix.net/name_api?name=' + name
+    );
+    var data = await response.json();
+
+    res.innerHTML = data;
+}
